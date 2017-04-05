4faf7fca32454978ef71e73f490a132298801100
Jarven <769558579@qq.com>
2017-04-05 14:49:16 +0800

    重构系统角色，用户

--- src/main/java/com/hcon/api/controller/SysRoleController.java
+++ src/main/java/com/hcon/api/controller/SysRoleController.java
@@ -1,5 +1,6 @@
 package com.hcon.api.controller;
 
+import com.hcon.api.domain.SysRole;
 import com.hcon.core.common.DataRet;
 import com.hcon.core.common.GridDataRet;
 import io.swagger.annotations.*;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("role")
 @Api(value = "平台角色接口")
-public class PubUserRoleController {
+public class SysRoleController {
 
 
     @ApiOperation(value = "查询角色列表")
@@ -24,14 +25,14 @@ public class PubUserRoleController {
             @ApiImplicitParam(value = "每页条数", name = "pageRows", required = true, dataType = "int"),
             @ApiImplicitParam(value = "搜索关键字", name = "key", dataType = "String")})
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public GridDataRet<PubUserRole> list(EqlPage eqlPage, String key) {
+    public GridDataRet<SysRole> list(EqlPage eqlPage, String key) {
         return null;
     }
 
 
     @ApiOperation(value = "创建角色")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public DataRet<String> create(@RequestBody PubUserRole userRole) {
+    public DataRet<String> create(@RequestBody SysRole sysRole) {
         return null;
     }
 
